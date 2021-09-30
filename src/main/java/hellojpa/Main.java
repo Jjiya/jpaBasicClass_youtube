727bcbe3cf7c6aaebefc80e99319abe82ffc0f2f
kim_hyeji <gpwl9610@gmail.com>
2021-09-30 13:05:07 +0900

    JPA 강의 6강 & 7강 1/2 완료

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -40,19 +40,55 @@ public class Main {
 //			양방향 매핑 시 주의할 점 위의 member.setTeam()을 실행하지 않고 연관관계의 역방향에 값을 주입할 경우
 			team.getMembers().add(member);
 			
-//			em.flush();	// DB에 즉시 반영하게 됨
-//			em.clear();	// 영속성 컨텍스트 안에 있는 캐시를 삭제
-//			3. 회원 조회
+			em.flush();	// DB에 즉시 반영하게 됨
+			em.clear();	// 영속성 컨텍스트 안에 있는 캐시를 삭제
 			
+//			member.setTeam(team);
+			
+			Member findMember = em.find(Member.class, member.getId());
+			
+//			em.close();	//영속성 종료
+			
+			Team findTeam = findMember.getTeam();
+			System.out.println(findTeam.getName());
+			System.out.println(findTeam + " >>>> find Team 결과");
+//			findMember.setName("로플랩");
+			
+//			3. 회원 조회
 //			String jpql = "select m from Member m join fetch m.team";
-			String jpql = "select m from Member m where m.username like '%h%'";	//왜 안될까?
+			
+			/*	JPQL 쿼리 규칙
+			 * 1. 엔티티와 속성은 대/소문자를 구분한다.
+			 * 2. JPQL 키워드는 대소문자 구분을 하지 않음(select from where 등...)
+			 * 3. 엔티티 이름을 사용, 테이블 사용이아님 (Member 클래스 명을 써야함)
+			 * 4. 별칭은 필수로 사용해야 한다. 
+			 * */
+			String jpql = "select m from Member m where m.username like '%h%'; ";	//왜 안될까?
+
+			/* parameter 바인딩 하는 법 (이름 기준, 위치 기준)
+			 * 순서보단 이름으로 바인딩 하는 것을 추천!!
+			 * 
+			 * 이름 기준 예시: SELECT m FROM Member m WHERE m.username =: username ;
+			 * 바인딩: query.setParameter("username", usernameParam);
+			 * 
+			 * 위치 기준 예시: SELECT m FROM Member m WHERE m.username =: ?1 ;
+			 * 바인딩: query.setParameter(1, usernameParam);
+			 * */
 			List<Member> members = em.createQuery(jpql, Member.class).getResultList();
+			em.flush();	// DB에 즉시 반영하게 됨
+			em.clear();	// 영속성 컨텍스트 안에 있는 캐시를 삭제
+			
+			/*
+			 * query.getResultList() 결과가 하나 이상 리스트로 반환될 때
+			 * query.getSingleResult() 결과가 딱 하나, 단일 객체가 반환될 때, 하나가 아니면 예외 발생함
+			 * */
+			System.out.println("여기");
 			for(Member mem : members) {
 				System.out.println("username: " + mem.getName() + ", teamName " + mem.getTeam().getName());
 			}
 			
 //			Member findMember = em.find(Member.class, member.getId());
-//			findMember.setName("새로운 팀명");	//JPA가 자동으로 UPDATE해줌 변경감지(Dirty Checking) 스냅샷으로 비교해서 업데이트
+			findMember.setName("새로운 팀명");	//JPA가 자동으로 UPDATE해줌 변경감지(Dirty Checking) 스냅샷으로 비교해서 업데이트
 			
 			tx.commit();
 			
